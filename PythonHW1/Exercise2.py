4db13e50052b1acad25522a5518111ddccbe5102
Alina <alinakozlova908@gmail.com>
2022-09-30 16:29:20 +0300

    the second exercise has changed, but it still doesn't work completely :')

--- PythonHW1/Exercise2.py
+++ PythonHW1/Exercise2.py
@@ -1,19 +1,15 @@
 print("X      Y      Z")
 print("___________________________")
-def truth_table(x, y, z):
-    if not(x or y or z) == (not x) or (not y) or (not z):
-        print(x,"\t", y,"\t", z, " = True")
-    else:
-        print (x,"\t", y,"\t", z, " = False")
+def truth_table():
+    for z in range(2):
+        for y in range(2):
+            for x in range(2):
+                if not(x or y or z) == (not x) or (not y) or (not z):
+                    print(x,"\t", y,"\t", z, " = True")
+                else:
+                    print (x,"\t", y,"\t", z, " = False")
 
-truth_table(True, True, True)
-truth_table(True, True, False)
-truth_table(False, False, False)
-truth_table(False, True, False)
-truth_table(True, False, False)
-truth_table(False, True, True)
-truth_table(False, False, True)
-truth_table(True, False, True)
+truth_table()
 
 
 
