f4e7f5b846b70400ce2c63dc0ab73b2a7e4fed0c
Alina <alinakozlova908@gmail.com>
2022-10-06 19:42:04 +0300

    the last exercise is unfinished

---
+++ PythonHW3/Exercise35.py
@@ -0,0 +1,33 @@
+# Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+# Пример:
+# - для k = 8 список будет выглядеть так: 
+# [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+#else return Fibonacci(n-1) + Fibonacci(n-2);
+
+def fibonacci(num):
+    fibonacci_list = []
+    if num == 2 or num == 1:
+        return 1
+    else:
+        return fibonacci(num-1) + fibonacci(num-2)
+        #fibonacci_list.append(num)
+    #print (fibonacci_list)
+
+fibonacci(5)
+
+for i in range(5):
+    print(fibonacci(i))
+
+
+#Аналог из С#
+# double Fibonacci(int n)
+# {
+#     if(n==1||n==2) return 1;
+#     else return Fibonacci(n-1) + Fibonacci(n-2);
+# }
+
+# for (int i = 1; i < 50; i++)
+# {
+#     Console.WriteLine($"(f{i}) = {Fibonacci(i)}");
+# }
